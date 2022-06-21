d032981b50be1e69ab0ea51c4003bac0ff471fad
Max Gravitt <max@digitalscarcity.io>
2022-06-21 18:12:15 -0400

    remove blog

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -52,7 +52,7 @@ const config = {
           src: 'img/logo.png',
         },
         items: [
-         {to: '/blog', label: 'Blog', position: 'left'},
+        //  {to: '/blog', label: 'Blog', position: 'left'},
         ],
       },
       footer: {
